e0740850d733a449e7c4a27bd4e9f527b11c20f1
Trevor Little <trevor.little@livingsocial.com>
2015-02-24 20:26:53 -0500

    move tests in with answer

--- src/four_clojure/p54.clj
+++ src/four_clojure/p54.clj
@@ -1,13 +1,21 @@
-(ns four-clojure.p54)
+(ns four-clojure.p54
+  (:require [clojure.test :refer :all]))
 
-(defn answer
-  [col]
-  (loop [col col, seen #{}, result []]
-    (if (seq col)
-      (recur
-       (rest col)
-       (conj seen (first col))
-       (if (seen (first col))
-         result
-         (conj result (first col))))
-      result)))
+(with-test
+
+  (defn answer
+    [col]
+    (loop [col col, seen #{}, result []]
+      (if (seq col)
+        (recur
+        (rest col)
+        (conj seen (first col))
+        (if (seen (first col))
+          result
+          (conj result (first col))))
+        result)))
+
+  (is (= (answer [1 2 1 3 1 2 4]) [1 2 3 4]))
+  (is (= (answer [:a :a :b :b :c :c]) [:a :b :c])) 
+  (is (= (answer '([2 4] [1 2] [1 3] [1 3])) '([2 4] [1 2] [1 3])))
+  (is (= (answer (range 50)) (range 50)))) 
--- test/four_clojure/p54_test.clj
+++
@@ -1,9 +0,0 @@
-(ns four-clojure.p54-test
-  (:require [clojure.test :refer :all]
-            [four-clojure.p54 :refer :all]))
-
-(deftest answer-test
-  (is (= (answer [1 2 1 3 1 2 4]) [1 2 3 4]))
-  (is (= (answer [:a :a :b :b :c :c]) [:a :b :c])) 
-  (is (= (answer '([2 4] [1 2] [1 3] [1 3])) '([2 4] [1 2] [1 3])))
-  (is (= (answer (range 50)) (range 50))))
