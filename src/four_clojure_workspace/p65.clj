02808f77236512fa4c8d27a2cb4d216f16c6813a
Trevor Little <TrevorMG19@gmail.com>
2015-03-03 11:41:30 -0500

    fix whitespace

--- src/four_clojure_workspace/p65.clj
+++ src/four_clojure_workspace/p65.clj
@@ -11,9 +11,9 @@
       (let [; maps are a function mapping their keys to values
             is-map?    (fn [c] (= 8 (get (conj c [9 8]) 9)))
             ; lists conj onto the front
-            is-list?   (fn [c] (= 2 (first (-> c  (conj 1)  (conj 2)))))
+            is-list?   (fn [c] (= 2 (first (-> c (conj 1) (conj 2)))))
             ; vectors conj onto the back
-            is-vector? (fn [c] (= 1 (first (-> c  (conj 1)  (conj 2)))))
+            is-vector? (fn [c] (= 1 (first (-> c (conj 1) (conj 2)))))
             ; sets only have one of each item
             is-set?    (fn [c] (= 1 (count (filter #(= 1 %) (conj c 1 1)))))]
         (cond
